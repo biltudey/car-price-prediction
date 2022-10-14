06552966f4f8c3715079836139b2db4582c098e2
Biltu Dey <94779899+biltudey@users.noreply.github.com>
2022-10-14 18:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,6 @@ def submit():
 
     return render_template('index.html',submit_value=output,car=car)
 
-
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
